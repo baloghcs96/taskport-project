8aaafd865dcd10ffcf057aa1d3ae93f14547d85c
baloghcs96 <balogh.csaba.6991@gmail.com>
2024-10-01 09:47:03 +0200

    first commit

---
+++ src/main/java/hu/taskport/taskport/TaskportApplication.java
@@ -0,0 +1,13 @@
+package hu.taskport.taskport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/taskport/taskport/TaskportApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.taskport.taskport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
